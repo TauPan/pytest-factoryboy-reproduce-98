2f692abd2ca4e991bf0e15127375e81918af0c84
Friedrich Delgado <delgado@dfn-cert.de>
2020-10-13 12:54:17 +0200

    add a simple model

--- django_project/models.py
+++ django_project/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Thing(models.Model):
+
+    a = models.IntegerField(default=1)
+    b = models.IntegerField(default=2)
+    sum_ = models.IntegerField()
+
+    def calculate_sum(self):
+        self.sum_ = self.a + self.b
+        self.save()
